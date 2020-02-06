88f4efa1edecf5c3ff81ba035ca46308a5318eb5
Melle de Vries <chadsv9000@gmail.com>
2020-02-06 09:19:08 +0100

    Added comments
    Meer informatie toegevoegd aan Program.cs over wat classes en instances zijn.

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -2,12 +2,25 @@
 
 namespace Game
 {
+    // Program is een class.
+    // Van een class kunnen we "instances" maken.
+    // Dit doen we op dezelfde manier als we bijvoorbeeld een nieuwe int aan zouden maken:
+    // type     | naam       = | value         
+    //----------+------------------------------
+    //  int       newInteger =   0;            
+    //  Program   newProgram =   new Program();
+    // Een class kan variabelen, methods, en nog veel meer bevatten.
+    // Al deze dingen heeft een instance van die class dan ook.
     class Program
     {
+        // De Main method is de ingang van onze code.
+        // Hier begint ons programma.
         static void Main(string[] args)
         {
             // Hier maken we een instance van de Game class.
+            // Een instance 
             Game Game = new Game();
+            // Daarna roepen we de Run method van de
             Game.Run();
         }
     }

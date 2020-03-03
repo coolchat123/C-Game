f89ac378c88fa737418f7404a04ac7ad7f3884bd
Apster <coolchat123@hotmail.com>
2020-03-03 09:27:10 +0100

    testing database, added conection string

---
+++ Game/Game/GameLoops/Games/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace Game.GameLoops.Games
+{
+    class Database
+    {
+        string conts = "Data Source=(LocalDB)/MSSQLLocalDB;AttachDbFilename=C:/xampp/htdocs/C-Game/Game/Game/Database/Database1.mdf;Integrated Security=True;Connect Timeout=30";
+
+    }
+}
+

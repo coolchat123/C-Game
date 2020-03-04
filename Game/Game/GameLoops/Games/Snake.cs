8b3e4dae8ff7fd8a65dde8d9a41ebed102284320
patreoth <55839525+patreoth@users.noreply.github.com>
2020-03-04 13:46:35 +0100

    grid area

--- Game/Game/GameLoops/Games/Snake.cs
+++ Game/Game/GameLoops/Games/Snake.cs
@@ -7,10 +7,19 @@ namespace Game
 {
     public class Snake : GameLoop
     {
+        static bool[,] Grid;
+
         public Snake() : base() { }
 
         public override void LoadContent()
         {
+            Grid = new bool[24,20]; 
+
+            for(int i = 0; i < 24; i++){
+                for(int j = 0; j < 20; j++){
+                    Grid[i,j]= false;    
+                }
+            } 
         }
 
         public override void Initialise()

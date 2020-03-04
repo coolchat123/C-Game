04b4b0a839c30b48547a8a88be0f72c0ee2f8b57
damianvlegels <djwvlegels@gmail.com>
2020-03-04 10:07:18 +0100

    Update Pong.cs

--- Game/Game/GameLoops/Games/Pong.cs
+++ Game/Game/GameLoops/Games/Pong.cs
@@ -1,23 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using SFML.Graphics;
 namespace Game
 {
     public class Pong : GameLoop
     {
+        static SSprite LeftPaddle;
+
+        static SSprite RightPaddle;
+
+        static SSprite Ball;
         public Pong() : base() { }
 
         public override void LoadContent()
         {
+
         }
 
         public override void Initialise()
         {
+
         }
 
         public override void Update(GameTime gameTime)
         {
+
         }
     }
 }

3d7c6a468f3b9393ae2bf03ada54556ef5f8040c
Apster <coolchat123@hotmail.com>
2020-03-09 13:13:56 +0100

    Update Pacman.cs

--- Game/Game/GameLoops/Games/Pacman.cs
+++ Game/Game/GameLoops/Games/Pacman.cs
@@ -7,10 +7,16 @@ namespace Game
 {
     public class Pacman : GameLoop
     {
+
+        int Life = 2;
+        int Score = 0;
+        static SSprite Map;
         public Pacman() : base() { }
 
         public override void LoadContent()
         {
+            Map = new SSprite(new Texture("Content/Pacman/Map.png"));
+            Map.Position = new Vector2f(10, Program.Window.Size.Y / 2 - Map.Texture.Size.Y * Program.Scale / 2);
         }
 
         public override void Initialise()
@@ -23,6 +29,28 @@ namespace Game
 
         public override void KeyInput(Keyboard.Key key)
         {
+            if (key == Keyboard.Key.W)
+            {
+                Console.WriteLine("test");
+                
+            }
+        }
+
+        public bool CheckCollision(float x, float y, float x2, float y2)
+        {
+            bool canMove = true;
+            for (float xCheck = x; xCheck < x2; xCheck++)
+            {
+                for (float yCheck = y; yCheck < y2; yCheck++)
+                {
+                    if (CollisionMap.GetPixel((uint)xCheck, (uint)yCheck) == Color.Blue)
+                    {
+                        canMove = false;
+                        Console.WriteLine("worksss");
+                    }
+                }
+            }
+            return canMove;
         }
     }
 }

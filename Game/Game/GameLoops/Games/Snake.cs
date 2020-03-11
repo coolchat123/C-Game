60d9271c3e67b9f637aa7302fa124adef140a71d
patreoth <55839525+patreoth@users.noreply.github.com>
2020-03-11 10:43:36 +0100

    Apple

--- Game/Game/GameLoops/Games/Snake.cs
+++ Game/Game/GameLoops/Games/Snake.cs
@@ -28,6 +28,8 @@ namespace Game
 
         static Button Return;
 
+        static SSprite Apple;
+
         public Snake() : base() { }
 
         public override void LoadContent()
@@ -53,8 +55,11 @@ namespace Game
             Return.MouseEnter += ReturnEnter;
             Return.MouseLeave += ReturnLeave;
 
-            SnakeHead = new SSprite(new Texture("content/snake/snakebox.png"));
+            SnakeHead = new SSprite(new Texture("content/snake/snakeHead.png"));
             SnakeHead.Position = new SFML.System.Vector2f(210, 20);
+
+            Apple = new SSprite(new Texture("content/snake/apple.png"));
+            Apple.Position = new SFML.System.Vector2f(50,80);
 
             Direction = "left";
 
@@ -88,12 +93,30 @@ namespace Game
                 Timer -= 1;
             }
 
+            if (SnakeHead.Position == Apple.Position)
+            {
+                scoreFunction:
+                float[] x_positions = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220 };
+                float[] y_positions = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190 };
+                float x = SnakeHead.Position.X;
+                float y = SnakeHead.Position.Y;
+                Random rnd = new Random();
+
+                x = x_positions[rnd.Next(0, 22)];
+                y = y_positions[rnd.Next(0, 19)];
+
+                if (SnakeHead.Position.X == x && SnakeHead.Position.Y == y)
+                    goto scoreFunction;
+
+                Apple.Position = new SFML.System.Vector2f(x, y);
+                Console.WriteLine("got one");
+                SetScore();
+            }
 
         }
 
         public override void KeyInput(Keyboard.Key key)
         {
-            SetScore();
 
             if ((key == Keyboard.Key.A || key == Keyboard.Key.Left) && Direction != "right")
             {
@@ -173,10 +196,10 @@ namespace Game
 
         public bool GameOverCheck()
         {
-            if (SnakeHead.Position.X <= -1 || SnakeHead.Position.Y <= -1 || SnakeHead.Position.X >=211 || SnakeHead.Position.Y >= 181)
+            if (SnakeHead.Position.X <= -1 || SnakeHead.Position.Y <= -1 || SnakeHead.Position.X >=221 || SnakeHead.Position.Y >= 191)
 
             {
-                Console.WriteLine("test");
+                Console.WriteLine("game over");
                 return true;
             }
 
@@ -190,7 +213,10 @@ namespace Game
 
         public void GameOver()
         {
-            
+            if (GameOverCheck())
+            {
+                
+            }
         }
 
     }
